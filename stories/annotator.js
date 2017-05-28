d50fbba3d46f6d62ca3274a02285a5d53809f961
Carlos Saito <c.saito.style@gmail.com>
2017-05-28 11:05:37 +0200

    Basic usage of Annotator (no action)

--- stories/annotator.js
+++ stories/annotator.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
+import Highlighter from '../src/Highlighter';
 
-storiesOf('Highlighter', module).add('1', () => <button />);
+storiesOf('Highlighter', module).add('1', () => (
+  <Highlighter>
+    This is a text <em>With an emphashis</em> inside
+  </Highlighter>
+));
 
 storiesOf('TaggerTooltip', module).add('1', () => <button />);
 
