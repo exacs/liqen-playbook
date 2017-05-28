0a92be6a0ad53f04784c1020c1a7390be4775b3e
Carlos Saito <c.saito.style@gmail.com>
2017-05-28 11:10:10 +0200

    2nd story: Highlighter with pre-selected text

--- stories/annotator.js
+++ stories/annotator.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Highlighter from '../src/Highlighter';
 
-storiesOf('Highlighter', module).add('1', () => (
-  <Highlighter>
-    This is a text <em>With an emphashis</em> inside
-  </Highlighter>
-));
+storiesOf('Highlighter', module)
+  .add('dumb', () => (
+    <Highlighter>
+      This is a text <em>With an emphashis</em> inside
+    </Highlighter>
+  ))
+  .add('with pre-selected text', () => (
+    <Highlighter
+      fragment={{
+        prefix: 'This is a text with ',
+        exact: 'an emphasis',
+        suffix: ' inside'
+      }}
+    >
+      This is a text <em>With an emphashis</em> inside
+    </Highlighter>
+  ));
 
 storiesOf('TaggerTooltip', module).add('1', () => <button />);
 
