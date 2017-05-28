1ccb377e2f152d1ed48cb3ace08ea490cd72d401
Carlos Saito <c.saito.style@gmail.com>
2017-05-28 10:54:13 +0200

    Showcase

---
+++ stories/annotator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+
+storiesOf('Highlighter', module)
+  .add('1', () => (
+    <button></button>
+  ))
+
+storiesOf('TaggerTooltip', module)
+  .add('1', () => (
+    <button></button>
+  ))
+
+storiesOf('AnnotationSelector', module)
+  .add('1', () => (
+    <button></button>
+  ))
+
+storiesOf('Annotator', module)
+  .add('1', () => (
+    <button></button>
+  ))
