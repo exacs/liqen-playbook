e7a3f7bdc91d323d42def7acd105b6632672c167
Carlos Saito <c.saito.style@gmail.com>
2017-05-30 00:28:35 +0200

    Add a third chapter of stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ function loadStories() {
   require('../stories/index.js');
   require('../stories/highlighter-1.js');
   require('../stories/highlighter-2.js');
+  require('../stories/highlighter-3.js');
   // You can require as many stories as you need.
 }
 
---
+++ stories/highlighter-3.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Highlighter from '../src/Highlighter';
+
+class Story extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      read: {
+        prefix: '',
+        suffix: '',
+        exact: ''
+      },
+      write: {
+        prefix: '',
+        suffix: '',
+        exact: ''
+      }
+    };
+    this.handleHighlight.bind(this);
+  }
+
+  handleHighlight(obj) {
+    this.setState({ read: obj });
+  }
+
+  mark() {
+    this.setState(prev => ({
+      write: prev.read,
+      read: prev.read
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          <Highlighter
+            fragment={this.state.write}
+            onHighlight={obj => this.handleHighlight(obj)}
+          >
+            Children is an <em>array</em> of several elements
+          </Highlighter>
+        </p>
+        <p>
+          <span>The result:</span>
+          <div>Prefix: <mark>{this.state.read.prefix}</mark></div>
+          <div>Exact: <mark>{this.state.read.exact}</mark></div>
+          <div>Suffix: <mark>{this.state.read.suffix}</mark></div>
+          <button onClick={() => this.mark()}>Highlight it!!</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+storiesOf('In and out', module).add('Dumbs', () => <Story />);
