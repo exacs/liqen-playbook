7c9c0921b092be853359f908ab43ad7d7736c53e
Carlos Saito <c.saito.style@gmail.com>
2017-05-30 15:39:36 +0200

    Implement Tagger

--- src/TaggerTooltip.js
+++ src/TaggerTooltip.js
@@ -9,3 +9,31 @@
  * Outputs
  * - onTag()
  */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TaggerTooltip = ({ list, selected, onSelect, onUnselect }) => (
+  <div>
+    {selected
+      ? <div>
+          <div>
+            Selected: {list.filter(tag => tag.id === selected)[0].title}
+          </div>
+          <div onClick={() => onUnselect()}>Unselect</div>
+        </div>
+      : <ul>
+          {list.map(tag => (
+            <li key={tag.id} onClick={() => onSelect(tag.id)}>{tag.title}</li>
+          ))}
+        </ul>}
+  </div>
+);
+
+TaggerTooltip.propTypes = {
+  list: PropTypes.array,
+  selected: PropTypes.number,
+  onSelect: PropTypes.func,
+  onUnselect: PropTypes.func
+};
+
+export default TaggerTooltip;
---
+++ stories/tagger.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import TaggerTooltip from '../src/TaggerTooltip';
+
+class WithState extends React.Component {
+  constructor() {
+    super();
+    this.state = { selected: null };
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleUnselect = this.handleUnselect.bind(this);
+  }
+
+  handleSelect(id) {
+    this.setState({ selected: id });
+  }
+
+  handleUnselect() {
+    this.setState({ selected: null });
+  }
+
+  render() {
+    return (
+      <TaggerTooltip
+        list={[
+          { id: 1, title: 'Tag 1' },
+          { id: 2, title: 'Tag 2' },
+          { id: 3, title: 'Tag 3' }
+        ]}
+        selected={this.state.selected}
+        onSelect={id => this.handleSelect(id)}
+        onUnselect={() => this.handleUnselect()}
+      />
+    );
+  }
+}
+
+storiesOf('Tagger', module)
+  .add('nothing selected', () => (
+    <TaggerTooltip
+      list={[
+        { id: 1, title: 'Tag 1' },
+        { id: 2, title: 'Tag 2' },
+        { id: 3, title: 'Tag 3' }
+      ]}
+    />
+  ))
+  .add('number 1 selected', () => (
+    <TaggerTooltip
+      list={[
+        { id: 1, title: 'Tag 1' },
+        { id: 2, title: 'Tag 2' },
+        { id: 3, title: 'Tag 3' }
+      ]}
+      selected={1}
+    />
+  ))
+  .add('with state', () => <WithState />);
